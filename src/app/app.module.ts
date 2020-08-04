bbec28228c66e533a297db2cfb9c3b84d3e2e2cf
Felipe Pepe <FelipePepe@gmail.com>
2020-08-04 04:35:27 +0200

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicMenuComponent } from './layout/public-menu/public-menu.component';
+import { PrivateMenuComponent } from './layout/private-menu/private-menu.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ContainerComponent } from './layout/container/container.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicMenuComponent,
+    PrivateMenuComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
